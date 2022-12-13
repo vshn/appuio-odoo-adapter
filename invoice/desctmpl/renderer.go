3d6bdc75fe4cbe8bafc2b29122ecb229e6f7d266
Fabian Fischer <glrf@users.noreply.github.com>
2022-12-13 17:44:28 +0100

    Add option to use one tempalte for multiple products

--- invoice/desctmpl/renderer.go
+++ invoice/desctmpl/renderer.go
@@ -41,13 +41,24 @@ func ItemDescriptionTemplateRendererFromFS(fs fs.FS, extension string) (*ItemDes
 
 // RenderItemDescription renders an item description. Uses the `.ProductRef.Source` as the key to look which template to use.
 func (r *ItemDescriptionTemplateRenderer) RenderItemDescription(_ context.Context, item invoice.Item) (string, error) {
-	key := item.ProductRef.Source
-	tmpl := r.root.Lookup(key + r.extension)
-	if tmpl == nil {
-		return "", fmt.Errorf("failed to find template for `ProductRef.Source=%q`%s", key, r.root.DefinedTemplates())
+	tmpl, err := r.lookup(item.ProductRef.Source)
+	if err != nil {
+		return "", err
+	}
+	b := &strings.Builder{}
+	err = tmpl.Execute(b, item)
+	return b.String(), err
+}
+
+func (r *ItemDescriptionTemplateRenderer) lookup(key string) (*template.Template, error) {
+
+	segments := strings.Split(key, ":")
+	for i := len(segments); i > 0; i-- {
+		tmpl := r.root.Lookup(strings.Join(segments[:i], ":") + r.extension)
+		if tmpl != nil {
+			return tmpl, nil
+		}
 	}
 
-	b := &strings.Builder{}
-	err := r.root.ExecuteTemplate(b, item.ProductRef.Source+r.extension, item)
-	return b.String(), err
+	return nil, fmt.Errorf("failed to find template for `ProductRef.Source=%q`%s", key, r.root.DefinedTemplates())
 }
