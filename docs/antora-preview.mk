52bd79ff4c6a365bac9ef7f77f10e43953b68a4c
wejdross <lukaszwidera1993@gmail.com>
2023-05-05 09:48:54 +0200

    Update docs/antora-preview.mk
    Co-authored-by: Sebastian Widmer <sebastian.widmer@vshn.net>

--- docs/antora-preview.mk
+++ docs/antora-preview.mk
@@ -1,5 +1,5 @@
 
-antora_preview_version ?= 2.3.14
+antora_preview_version ?= 3.1.2.3
 antora_preview_cmd ?= $(DOCKER_CMD) run --rm --publish 2020:2020 --volume "${PWD}":/preview/antora ghcr.io/vshn/antora-preview:latest --style=vshn
 
 .PHONY: docs-preview
